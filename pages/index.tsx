136431578251970f9318435f851320b2e1cab7dc
han_kyeul <phk9436phk@gmail.com>
2023-04-05 22:32:25 +0900

    nextjs getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,29 @@ import { useEffect, useState } from "react";
 import { getMovies } from "../components/Api";
 import { useQuery } from "@tanstack/react-query";
 import { ImovieData } from "../components/Api";
+import axios from "axios";
 
-function Home() {
-  const [movies, setMovies] = useState<ImovieData[]>([]);
-  const { isLoading, data } = useQuery(["movies", "popular"], getMovies);
-  console.log(movies);
+interface Iporps {
+  results: ImovieData[];
+}
 
-  useEffect(() => {
-    data && setMovies(data.data.results);
-  }, [isLoading]);
+function Home({ results }: Iporps) {
   return (
     <div>
-      {isLoading
-        ? "Loading..."
-        : movies.map((e) => (
-            <div key={e.id}>
-              <h4>{e.title}</h4>
-            </div>
-          ))}
+      {results.map((e) => (
+        <div key={e.id}>
+          <h4>{e.title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const {
+    data: { results },
+  } = await axios.get("http://localhost:3000/api/movies/popular");
+  return { props: { results } };
+};
